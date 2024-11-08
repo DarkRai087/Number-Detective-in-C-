8ed8dd8efcd170a54fa12e23400618a66073c96a
Parth Pandey <parthp90111@gmail.com>
2024-11-08 18:49:17 +0530

    Test v.1

--- program.cpp
+++ program.cpp
@@ -2,3 +2,26 @@
 #include <sstream>
 #include <string>
 #include <cmath>
+
+using namespace std;
+
+bool isInt(double num)
+{
+    return floor(num) == num;
+}
+int main()
+{
+
+    double num;
+    cout << "Enter a Number : ";
+    cin >> num;
+
+    if (isInt(num))
+    {
+        cout << "- An Integer" << endl;
+    }
+    else
+    {
+        cout << "Invalid input" << endl;
+    }
+}
