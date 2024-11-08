05e9a81d6393d7fd42092ba4354ea99f56482a2f
Parth Pandey <parthp90111@gmail.com>
2024-11-08 20:01:37 +0530

    Checking for a Fraction

--- program.cpp
+++ program.cpp
@@ -9,19 +9,47 @@ bool isInt(double num)
 {
     return floor(num) == num;
 }
+bool isRational(const string &input)
+{
+    return input.find('/') != string::npos;
+}
+bool isNatural(double num)
+{
+    return isInt(num) && num > 0;
+}
+bool isWhole(double num)
+{
+    return isInt(num) && num >= 0;
+}
+bool isIrrational(double num)
+{
+    return !isInt(num) && !isRational(to_string(num));
+}
+
 int main()
 {
-
+    string input;
     double num;
     cout << "Enter a Number : ";
-    cin >> num;
-
-    if (isInt(num))
+    cin >> input;
+    if (isRational(input))
     {
-        cout << "- An Integer" << endl;
+        istringstream ss(input);
+        double numerator, denominatior;
+        char slash;
+        ss >> numerator >> slash >> denominatior;
+        if (slash == '/' && denominatior != 0)
+        {
+            num = numerator / denominatior;
+        }
+        else
+        {
+            cout << " Invalid input " << endl;
+            return 1;
+        }
     }
     else
     {
-        cout << "Invalid input" << endl;
+        num = stod(input);
     }
 }
